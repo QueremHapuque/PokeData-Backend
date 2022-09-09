f14cc04cc5342abad8f75b8f52e2023988536c51
Diegod064 <diegodcg064@gmail.com>
2022-09-09 15:15:53 -0300

    Crição da api allstatusofpokemon

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app.config['JSON_SORT_KEYS'] = False
 
 dsP = pd.read_parquet('pokemon.parquet')
 dsP = pokeId(dsP)
-print(dsP)
+# print(dsP)
 
 ds = open('pokemon.json')
 dsjson = json.load(ds)
@@ -76,5 +76,25 @@ def alladvantageofpokemon(tipo):
     return make_response(
         jsonify(vantagens)
     )
+
+@app.route('/status/<string:id>', methods=['GET'])
+def allstatusofpokemon(id):
+    pokemon = dsP.loc[dsP['id'] == str(id)].to_dict()
+    
+    pokemon_status = [
+        {
+            "hp": pokemon['hp'],
+            "attack":pokemon['attack'],
+            "defense": pokemon['defense'],
+            "speed": pokemon['speed'],
+            "special_attack": pokemon['special_attack'],
+            "special_defense": pokemon['special_defense'],
+        }
+    ]
+
+    return make_response(
+        jsonify(pokemon_status)    
+    )
+
 
 app.run()
